2423a63436b51d33de5970c7c588e0d04b02b4bf
Rami Rostom <rami.rostom@proton.me>
2023-12-22 16:04:43 +0100

    :wrench: config : init server + config express

---
+++ server.js
@@ -0,0 +1,13 @@
+require ('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
