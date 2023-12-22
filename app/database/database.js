6387e97cc93c7513289f19883e21e82851a99397
Rami Rostom <rami.rostom@proton.me>
2023-12-22 16:11:40 +0100

    :wrench: config : database config with sequelize

---
+++ app/database/database.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PG_URL, {
+  dialect: 'postgres',
+  define: { underscored: true }
+});
+
+module.exports = sequelize;
