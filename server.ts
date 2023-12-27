cf90eebe74480047e7d91638c5457f1b736b748a
Rami Rostom <rami.rostom@proton.me>
2023-12-27 12:19:25 +0100

    :wrench: config: install and config typescript

--- server.ts
+++ server.ts
@@ -4,12 +4,12 @@ const express = require('express');
 const router = require('./app/router');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
