562c7609f3caa648d9c8686178e3135087301f9f
Rami <rami.rostom@proton.me>
2024-01-16 14:02:04 +0100

    :sparkles: feat: change timezone in sequelize configuration to avoid issues with dates value

--- app/database/database.js
+++ app/database/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PG_URL, {
   dialect: 'postgres',
-  define: { underscored: true }
+  define: { underscored: true },
+  timezone: 'Europe/Paris',
 });
 
 module.exports = sequelize;
