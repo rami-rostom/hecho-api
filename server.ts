0e96187889ff52ea090ae8e2d97660da166b46bd
Rami <rami.rostom@proton.me>
2024-02-06 21:13:46 +0100

    :wrench: config: edit cors policy

--- server.ts
+++ server.ts
@@ -12,22 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:4173',
+    origin: '*',
     credentials: true,
   })
 );
-app.use(
-  cors({
-    origin: 'https://hecho-app.onrender.com',
-    credentials: true,
-  })
-);
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'https://hecho-app.onrender.com',
+//     credentials: true,
+//   })
+// );
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+//   })
+// );
 app.use(router);
 
 app.listen(PORT, () => {
