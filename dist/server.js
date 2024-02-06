f1b1cd069e9358d4c702e984f27d695ee73a08ce
Rami <rami.rostom@proton.me>
2024-02-06 20:59:15 +0100

    :wrench: config: edit build script

--- dist/server.js
+++ dist/server.js
@@ -13,6 +13,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use(cors({
+    origin: 'http://localhost:4173',
+    credentials: true,
+}));
+app.use(cors({
+    origin: 'https://hecho-app.onrender.com',
+    credentials: true,
+}));
+app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
